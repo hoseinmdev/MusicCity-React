023fad321205eb9ba8d100578fe0d665c284f8cc
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:31:19 +0330

    feat: add index file

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+import ArtistAvatar from "./ArtistAvatar";
+import PlaylistBox from "./PlaylistBox";
+import SiteLogo from "./SiteLogo";
+import TrackBox from "./TrackBox";
+import TrackLine from "./TrackLine";
+
+export { ArtistAvatar, TrackBox, SiteLogo, PlaylistBox , TrackLine};

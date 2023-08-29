aaca8682d168cc2aedef9a91bd7cb0e21003556f
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:29:41 +0330

    fix: fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "@/sote.ts";
+import { store } from "@/store.ts";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(

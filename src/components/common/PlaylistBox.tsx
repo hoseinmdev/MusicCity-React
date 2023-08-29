5c78e26cc0a75435e6fbb55ee62e114ce33aef50
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:30:25 +0330

    implement track box

---
+++ src/components/common/PlaylistBox.tsx
@@ -0,0 +1,27 @@
+import { PLAYLIST_PAGE } from "@/pathes";
+import React from "react";
+import { Link } from "react-router-dom";
+
+type PlaylistBoxProps = {
+  title: string;
+  imageUrl: string;
+  followers: number;
+};
+
+const PlaylistBox: React.FC<PlaylistBoxProps> = ({
+  title,
+  imageUrl,
+  followers,
+}) => {
+  return (
+    <Link
+      to={`/${PLAYLIST_PAGE}/${title}`}
+      className="flex cursor-pointer flex-col items-center justify-between gap-2 text-base text-white hover:opacity-70 "
+    >
+      <img className="rounded-xl" src={imageUrl} alt="" />
+      <p className="text-sm lg:text-base">{title}</p>
+      <p className="opacity-60">followers: {followers + "k"}</p>
+    </Link>
+  );
+};
+export default PlaylistBox;

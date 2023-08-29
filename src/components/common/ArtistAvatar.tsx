9b7b3683859f5c23a66274e09a99d7279352a1f9
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:31:05 +0330

    feat: implement Artist avater

--- src/components/common/ArtistAvatar.tsx
+++ src/components/common/ArtistAvatar.tsx
@@ -1,4 +1,7 @@
+import { SINGLE_ARTIST_PAGE } from "@/pathes";
 import React from "react";
+import { useMediaPredicate } from "react-media-hook";
+import { Link } from "react-router-dom";
 
 type ArtistAvatarProps = {
   title: string;
@@ -11,12 +14,30 @@ const ArtistAvatar: React.FC<ArtistAvatarProps> = ({
   followers,
   imageUrl,
 }) => {
+  const isMobile = useMediaPredicate("(max-width: 1024px)");
+
   return (
-    <div className="flex flex-col items-center justify-center gap-1 text-white">
-      <img className="h-40 w-40 rounded-full hover:opacity-70" src={imageUrl} alt="" />
-      <p>{title}</p>
-      <p className="opacity-60">{followers}k followers</p>
-    </div>
+    <Link
+      to={`/${SINGLE_ARTIST_PAGE}/${title}`}
+      className="flex  flex-col items-center justify-between gap-2 text-sm text-white  lg:text-base"
+    >
+      <img
+        className="h-full w-full rounded-full hover:opacity-70"
+        src={imageUrl}
+        alt=""
+      />
+      {isMobile ? (
+        <p className="text-sm">
+          {title.length <= 10 ? title : title.slice(0, 9) + "... "}
+        </p>
+      ) : (
+        <p className="text-base">
+          {title.length <= 15 ? title : title.slice(0, 14) + "... "}
+        </p>
+      )}
+
+      <p className="text-xs opacity-60">{followers}k followers</p>
+    </Link>
   );
 };
 export default ArtistAvatar;

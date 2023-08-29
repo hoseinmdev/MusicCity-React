d708b22b5f20bba2782634e1065b82562388a636
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:23:44 +0330

    feat: implement browse page

---
+++ src/pages/BrowsePage.tsx
@@ -0,0 +1,48 @@
+import { TrackLine } from "@/components/common";
+import Skeleton from "@/components/common/Skeleton";
+import SiteLayout from "@/layout/SiteLayout";
+import { RootState } from "@/sote";
+import createEmptyArray from "@/utils/createEmptyArray";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+const BrowsePage: React.FC = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setShow(true), 1500);
+  });
+  const tracks = useSelector((state: RootState) => state.tracks.tracks);
+  return (
+    <SiteLayout>
+      {show ? (
+        <div className=" p-4 flex w-full flex-col items-start justify-start gap-5 pt-14 pb-24">
+          <p className="fixed left-0 top-0 z-30 flex w-full items-center justify-center rounded-b-2xl bg-neutral-800 p-2 text-lg lg:relative lg:justify-start lg:bg-transparent">
+            Songs for you :)
+          </p>
+          {tracks.map((track) => {
+            return (
+              <TrackLine
+                imageUrl={track.imageUrl}
+                musicName={track.musicName}
+                singer={track.singer}
+                key={track.imageUrl}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <BrosePageSkeleton />
+      )}
+    </SiteLayout>
+  );
+};
+const BrosePageSkeleton = () => {
+  return (
+    <div className="flex w-full flex-col gap-5 p-4 lg:gap-7 lg:pt-16">
+      {createEmptyArray(11).map((skeleton, index) => {
+        return <Skeleton key={index} className="h-14 w-full rounded-lg" />;
+      })}
+    </div>
+  );
+};
+export default BrowsePage;

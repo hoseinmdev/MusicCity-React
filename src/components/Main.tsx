e55a2595a31e4c3a4d77c0e2520380a32944d5d0
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:21:18 +0330

    implement main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,12 @@
-import { FC } from "react";
-import Slider from "./Slider";
-import ArtistAvatar from "./common/ArtistAvatar";
-import mohsenYeganeh from "../assets/Artists/Yas.jpg";
-const Main: FC = () => {
+import { FC, ReactNode } from "react";
+
+interface MainProps {
+  children: ReactNode;
+}
+const Main: FC<MainProps> = ({ children }) => {
   return (
-    <div className="h-screen w-full overflow-auto bg-neutral-900">
-      <Slider />
-      <ArtistAvatar
-        title="Mohsen Yeganeh"
-        followers={32}
-        imageUrl={mohsenYeganeh}
-      />
+    <div className="h-full w-full flex flex-col overflow-auto bg-neutral-900 ">
+      <div className="fadeShow h-full w-full text-white">{children}</div>
     </div>
   );
 };

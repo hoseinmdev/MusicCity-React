5a0202570955bbec9453768afb030f76a4266e41
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-29 11:24:40 +0330

    feat: implement pathes

---
+++ src/pathes.tsx
@@ -0,0 +1,17 @@
+const HOME_PAGE = "/"
+const ARTISTS_PAGE = "Artists"
+const TRACK_PAGE = "Track";
+const SEARCH_PAGE = "Search";
+const BROWSE_PAGE = "Browse";
+const PLAYLIST_PAGE = "Playlist";
+const SINGLE_ARTIST_PAGE = "SingleArtist";
+
+export {
+  HOME_PAGE,
+  ARTISTS_PAGE,
+  TRACK_PAGE,
+  SEARCH_PAGE,
+  BROWSE_PAGE,
+  PLAYLIST_PAGE,
+  SINGLE_ARTIST_PAGE,
+};

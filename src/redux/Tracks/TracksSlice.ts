8f95598d866fb790764c5310edf2d968e7543ad3
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-09-03 11:39:08 +0330

    feat: add tracks slice

--- src/redux/Tracks/TracksSlice.ts
+++ src/redux/Tracks/TracksSlice.ts
@@ -1,32 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { tracks } from "@/db/tracks";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-interface Track {
+export interface ITrack {
+  id: string;
   musicName: string;
   singer: string;
   imageUrl: string;
   url: string;
+  state?: string;
+  genre?: string;
 }
 export interface TracksState {
-  tracks: Track[];
+  tracks: ITrack[];
+  loading: boolean;
 }
+
+export const getTracks = createAsyncThunk("tracks/getTracks", async () => {
+  try {
+    const response = await axios.get(
+      "https://musiccity-api.netlify.app/tracks",
+    );
+    return response.data.data;
+  } catch (error) {
+    return [];
+  }
+});
+
 const initialState: TracksState = {
-  tracks: tracks,
+  tracks: [],
+  loading: false,
 };
 
 export const TrackSlice = createSlice({
-  name: "Tracks",
+  name: "allTracks",
   initialState,
-  reducers: {
-    logger: (state) => {
-      console.log(state);
-    },
-    //     incrementByAmount: (state, action: PayloadAction<number>) => {
-    //       state.value += action.payload;
-    //     },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getTracks.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getTracks.fulfilled, (state, action) => {
+      state.tracks = action.payload;
+      state.loading = false;
+    });
   },
 });
-
-export const { logger } = TrackSlice.actions;
-
 export default TrackSlice.reducer;

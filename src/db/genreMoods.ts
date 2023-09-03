7d02634e445d5fd8660bbec15026ff2063900645
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-09-03 11:40:09 +0330

    feat: add id for genres

--- src/db/genreMoods.ts
+++ src/db/genreMoods.ts
@@ -3,32 +3,33 @@ import Rap from "@/assets/SearchCategories/Rap.jpg";
 import Romance from "@/assets/SearchCategories/Romance.jpg";
 import Sad from "@/assets/SearchCategories/Sad.jpg";
 import Nostalgic from "@/assets/SearchCategories/Nostalgic.jpg";
+import { PLAYLIST_PAGE } from "@/pathes";
 export const genres = [
   {
     title: "Pop",
-    path: `/Playlist/Persian Pop`,
+    path: `/${PLAYLIST_PAGE}/e52a5568e17699c19dd55818ebd49500`,
     imageUrl: Pop,
   },
   {
     title: "Rap",
-    path: `/Playlist/Persian Rap`,
+    path: `/${PLAYLIST_PAGE}/c47f803da3186f5b98397a641d79ba86`,
     imageUrl: Rap,
   },
 ];
 export const moods = [
   {
     title: "Romantic",
-    path: `/Playlist/Romantic`,
+    path: `/${PLAYLIST_PAGE}/d5ded8664c9411bb4190136f7bb0e892`,
     imageUrl: Romance,
   },
   {
     title: "Sad",
-    path: `/Playlist/Sad Songs`,
+    path: `/${PLAYLIST_PAGE}/a41f720b9093682735763fb6e5dfce09`,
     imageUrl: Sad,
   },
   {
     title: "Nostalgia",
-    path: `/Playlist/Persian Nostalgia`,
+    path: `/${PLAYLIST_PAGE}/68357c8a8be073b4588e65cc32c1e522`,
     imageUrl: Nostalgic,
   },
 ];

f3c17eb85b2fe5aa7c8465eafffb71cc9ec30b80
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-09-06 22:11:16 +0330

    fix theme bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "@/store.ts";
 import { Provider } from "react-redux";
 
+const savedTheme = JSON.parse(localStorage.getItem("theme") || `[]`);
+
+if (savedTheme.length === 0) {
+  localStorage.setItem("theme", JSON.stringify("dark"));
+}
+
+savedTheme === "light"
+  ? document.documentElement.classList.remove("dark")
+  : document.documentElement.classList.add("dark");
+  
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>

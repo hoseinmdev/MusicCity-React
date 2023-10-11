08ded50c5ce030e45131c5f796c617749f9f7373
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-11 17:38:23 +0330

    update manifest plugin

--- pl.js
+++ pl.js
@@ -7,29 +7,29 @@
     description: "I am a simple vite app",
     icons: [
       {
-        src: "/android-chrome-192x192.png",
+        src: "/public/musicCityLogo.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "favicon",
       },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/maskable_icon.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
+      // {
+      //   src: "/android-chrome-512x512.png",
+      //   sizes: "512x512",
+      //   type: "image/png",
+      //   purpose: "favicon",
+      // },
+      // {
+      //   src: "/apple-touch-icon.png",
+      //   sizes: "180x180",
+      //   type: "image/png",
+      //   purpose: "apple touch icon",
+      // },
+      // {
+      //   src: "/maskable_icon.png",
+      //   sizes: "512x512",
+      //   type: "image/png",
+      //   purpose: "any maskable",
+      // },
     ],
     theme_color: "#171717",
     background_color: "#f0e7db",

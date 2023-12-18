31f7f2a33733b303eee3d1081bdb7f0191726a26
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-12-18 13:40:03 +0330

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {
-  AiFillInstagram,
-  AiFillTwitterCircle,
-  AiFillYoutube,
-} from "react-icons/ai";
-import { FaTelegram } from "react-icons/fa6";
+// import {
+//   AiFillInstagram,
+//   AiFillTwitterCircle,
+//   AiFillYoutube,
+// } from "react-icons/ai";
+// import { FaTelegram } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-4 p-4">
-      <div className="flex w-full items-center justify-start gap-3 text-xl ">
+      {/* <div className="flex w-full items-center justify-start gap-3 text-xl ">
         <button className="lg:text-white text-gray-800 hover:scale-110 hover:text-white hover:opacity-100 dark:text-white dark:opacity-50">
           <AiFillInstagram />
         </button>
@@ -30,7 +30,7 @@ const Footer: React.FC = () => {
         <button className="text-gray-800 hover:text-white hover:opacity-100 dark:text-white  dark:opacity-50 lg:text-white">
           hoseinmdev@gmail.com : ایمیل
         </button>
-      </div>
+      </div> */}
       <div className="flex w-full cursor-default flex-col items-center justify-center gap-1 border-t  border-gray-500 pt-2 text-sm text-gray-800 hover:text-white hover:opacity-100 dark:text-white dark:opacity-50 lg:text-white">
         <p>تمامی حقوق مادی و معنوی این سایت</p>
         <p>متعلق به حسین محمودی میباشد</p>
